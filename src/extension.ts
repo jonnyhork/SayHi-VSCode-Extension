f447c26167a257c75dbe480a468e9a1a3c25745c
Jonny Hork <jhork@salesforce.com>
2019-04-11 11:30:44 -0600

    Clean up comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 const sayWhatExports = vscode.extensions.getExtension("jh.saywhat")!.exports;
-const getMessage = sayWhatExports.getMessage;
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const getMessage = sayWhatExports.getMessageFn;
+
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "sayhi" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "sayhi" is now active!');
+  let displayMessage = vscode.commands.registerCommand(
+    "sayHi.displayMessage",
+    async () => {
+      // Display a message box to the user
+      let message = await getMessage();
+      vscode.window.showInformationMessage(`Hi ${message}!`);
+    }
+  );
 
-
-	let displayMessage = vscode.commands.registerCommand('sayHi.displayMessage', async () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		let message =  await getMessage();
-		vscode.window.showInformationMessage(`Hi ${message}!`);
-	});
-
-	context.subscriptions.push(displayMessage);
+  context.subscriptions.push(displayMessage);
 }
 
 // this method is called when your extension is deactivated
