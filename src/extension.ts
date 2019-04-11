87bd6eab3c0728e1b7a6b28f0a23ed062b49ca74
Jonny Hork <jhork@salesforce.com>
2019-04-11 16:22:03 -0600

    Move logic into command

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,26 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "sayhi" is now active!');
+const sayWhatExtension = vscode.extensions.getExtension("jh.saywhat")!;
 
-  const sayWhatExports = vscode.extensions.getExtension("jh.saywhat")!.exports;
-  const getMessage = sayWhatExports.getMessageFn;
+export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension -----"SAY HI"--- is now active!'
+  );
 
   let displayMessage = vscode.commands.registerCommand(
     "sayHi.displayMessage",
     async () => {
+      let getMessage: () => void;
+
+      if (sayWhatExtension.isActive) {
+        getMessage = sayWhatExtension.exports.getMessageFn;
+      } else {
+        let exports = await sayWhatExtension.activate();
+        getMessage = exports.getMessageFn;
+      }
       // Display a message box to the user
       let message = await getMessage();
+
       vscode.window.showInformationMessage(`Hi ${message}!`);
     }
   );
