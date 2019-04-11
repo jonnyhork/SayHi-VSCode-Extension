7633f8af883055903b82621c9f05e4714c376704
Jonny Hork <jhork@salesforce.com>
2019-04-11 14:20:21 -0600

    Move getExtension inside active()

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,10 @@
 import * as vscode from "vscode";
 
-const sayWhatExports = vscode.extensions.getExtension("jh.saywhat")!.exports;
-const getMessage = sayWhatExports.getMessageFn;
-
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "sayhi" is now active!');
+
+  const sayWhatExports = vscode.extensions.getExtension("jh.saywhat")!.exports;
+  const getMessage = sayWhatExports.getMessageFn;
 
   let displayMessage = vscode.commands.registerCommand(
     "sayHi.displayMessage",
